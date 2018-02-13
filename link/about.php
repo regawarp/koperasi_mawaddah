33fbb037d5706ea5564fcd3f3b33493fa3ad311d
Ilham Gibran <ilgibrz@gmail.com>
2018-02-13 15:29:29 +0700

    Edited
    ini diedit

--- link/about.php
+++ link/about.php
@@ -1,12 +1,9 @@
-<!--- 
-COBA DONG
-
- --><!DOCTYPE html>
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Koperasi Mawaddah</title>
+    <title>Koperasi Mawaddah :D</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	
